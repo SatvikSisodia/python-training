59a9a5c1db69aa73428e028a81a75d236bb4bff1
SatvikSisodia <sisodiasatvik1413@gmail.com>
2026-05-08 20:52:55 +0530

    finds sum of first n numbers

---
+++ SumOfFirstN_Numbers.py
@@ -0,0 +1,10 @@
+a=1
+b=2
+c=int(input("enter the number."))
+d=1
+print(f"Calculating the sum of natural numbers till {c}...")
+while (d<=c):
+    a+=b
+    b+=1
+    d+=1
+print(f"Sum of first {c} natural no.s is {a}")    
