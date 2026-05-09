e5c522b73e4e3caad30467cb50e46b60ee49b3d2
SatvikSisodia <sisodiasatvik1413@gmail.com>
2026-05-09 12:03:51 +0530

    used functions

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+a=False
+def calc():
+    operation=input("would you like to add, subtract, multiply or divide(Please write in smallcase)?")
+    if (operation=="add"):
+        print(f"The sum is {x+y}")
+        a=True
+    elif (operation=="subtract"):
+        print(f"The difference is {x-y}")
+        a=True
+    elif (operation=="multiply"):
+        print(f"The result is {x*y}")
+        a=True
+    elif (operation=="divide"):
+        print(f"The quotient is {x/y}")
+        a=True
+    else:
+        print("Invalid input")
+b=1
+while (b==1):
+    x=int(input("enter any number:"))
+    y=int(input("enter another number:"))
+    calc()
+    b=int(input("Press the number 1 if you want to continue with a new set of numbers, otherwise press any number."))
+print("Thank you")
